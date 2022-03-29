579f4c2b80bf9e1301a2230f26f87e320dd17926
lakshayr31 <lakshyarastogi31@gmail.com>
2022-03-29 12:18:41 +0530

    added configuration for put and delete methods as well

--- src/main/java/com/arpo/backend/CorsConfiguration.java
+++ src/main/java/com/arpo/backend/CorsConfiguration.java
@@ -26,7 +26,7 @@ public class CorsConfiguration
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**").allowedMethods("*");
             }
         };
     }
